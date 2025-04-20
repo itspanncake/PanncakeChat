34dee8663a8e02c84e2b325e3922ce98b4241a95
Plhume <72684309+Plhume@users.noreply.github.com>
2025-04-20 13:03:16 +0200

    Create singleton in main class

--- src/main/java/fr/panncake/chat/PanncakeChat.java
+++ src/main/java/fr/panncake/chat/PanncakeChat.java
@@ -2,15 +2,24 @@ package fr.panncake.chat;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+
 public class PanncakeChat extends JavaPlugin {
+
+    private static PanncakeChat instance;
 
     @Override
     public void onEnable() {
+        instance = this;
+
         getLogger().info("PanncakeChat has been enabled!");
     }
 
     @Override
     public void onDisable() {
         getLogger().info("PanncakeChat has been disabled!");
+    }
+
+    public static PanncakeChat getInstance() {
+        return instance;
     }
 }
