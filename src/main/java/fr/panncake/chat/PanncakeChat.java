b7f3b2226e2f4c43323645ccb5b4192b2431ba23
Plhume <72684309+itspanncake@users.noreply.github.com>
2025-04-20 13:20:01 +0200

    Create Redis connection

--- src/main/java/fr/panncake/chat/PanncakeChat.java
+++ src/main/java/fr/panncake/chat/PanncakeChat.java
@@ -1,5 +1,6 @@
 package fr.panncake.chat;
 
+import fr.panncake.chat.managers.RedisManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 
@@ -7,10 +8,19 @@ public class PanncakeChat extends JavaPlugin {
 
     private static PanncakeChat instance;
 
+    private RedisManager redisManager;
+
     @Override
     public void onEnable() {
         instance = this;
         saveDefaultConfig();
+
+        this.redisManager = new RedisManager(
+                getConfig().getString("redis.host"),
+                getConfig().getInt("redis.port"),
+                getConfig().getString("redis.password"),
+                getConfig().getInt("redis.timeout")
+        );
 
         getLogger().info("PanncakeChat has been enabled!");
     }
